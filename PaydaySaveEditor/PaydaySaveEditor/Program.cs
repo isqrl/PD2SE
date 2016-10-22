d61a15b841b348af01cec0b215dea61d70b0296b
rk <git@razlaw.name>
2016-10-22 10:12:38 +0200

    Cleanup unused using-directives

--- PaydaySaveEditor/PaydaySaveEditor/Program.cs
+++ PaydaySaveEditor/PaydaySaveEditor/Program.cs
@@ -2,11 +2,7 @@
 using CommandLine.Text;
 using PD2.GameSave;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Windows.Forms;
 
 namespace PD2
 {
