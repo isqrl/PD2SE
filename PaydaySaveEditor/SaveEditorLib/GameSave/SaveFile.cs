bdef09b514baa6fa23ddf01789230eee61e807de
rk <git@razlaw.name>
2016-10-22 12:31:26 +0200

    cleanup: using

--- PaydaySaveEditor/SaveEditorLib/GameSave/SaveFile.cs
+++ PaydaySaveEditor/SaveEditorLib/GameSave/SaveFile.cs
@@ -57,29 +57,22 @@ namespace PD2.GameSave
             byte[] data = encrypted ? Encryption.TransformData(file) : file;
 
             // Open save to process
-            BinaryReader br = new BinaryReader(new MemoryStream(data));
-
-            // Validate that file is a supported save format
-            int version = br.ReadInt32();
-            if (version != SAVE_VERSION)
+            using (BinaryReader br = new BinaryReader(new MemoryStream(data)))
             {
-                throw new Exception(String.Format("Unsupported save version: {0}", version.ToString("X4")));
+                // Validate that file is a supported save format
+                int version = br.ReadInt32();
+                if (version != SAVE_VERSION)
+                {
+                    throw new Exception(String.Format("Unsupported save version: {0}", version.ToString("X4")));
+                }
+
+                return new SaveFile(filePath)
+                {
+                    header = new DataBlock(br),
+                    gamedata = new GameDataBlock(br),
+                    footer = new DataBlock(br)
+                };
             }
-
-            // Process blocks
-            var header = new DataBlock(br);
-            var gamedata = new GameDataBlock(br);
-            var footer = new DataBlock(br);
-
-            // We're done with our reader
-            br.Close();
-
-            return new SaveFile(filePath)
-            {
-                header = header,
-                gamedata = gamedata,
-                footer = footer
-            };
         }
 
         public static void Save(SaveFile saveFile, bool encrypt = true)
@@ -89,25 +82,26 @@ namespace PD2.GameSave
 
         public static void Save(SaveFile saveFile, string outputPath, bool encrypt = true)
         {
-            MemoryStream ms = new MemoryStream();
-            BinaryWriter bw = new BinaryWriter(ms);
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (BinaryWriter bw = new BinaryWriter(ms))
+                {
+                    // Rebuild save
+                    bw.Write(SAVE_VERSION);
+                    bw.Write(saveFile.header.ToArray());
+                    bw.Write(saveFile.gamedata.ToArray());
+                    bw.Write(saveFile.footer.ToArray());
+                    bw.Write(new byte[16]);
+                    bw.Write(Encryption.GenerateSaveHash(ms.ToArray()));
 
-            // Rebuild save
-            bw.Write(SAVE_VERSION);
-            bw.Write(saveFile.header.ToArray());
-            bw.Write(saveFile.gamedata.ToArray());
-            bw.Write(saveFile.footer.ToArray());
-            bw.Write(new byte[16]);
-            bw.Write(Encryption.GenerateSaveHash(ms.ToArray()));
+                    // Store save and close streams
+                    byte[] data = ms.ToArray();
 
-            // Store save and close streams
-            byte[] data = ms.ToArray();
-            bw.Close();
-            ms.Close();
-
-            // Encrypt if requested
-            if (encrypt) data = Encryption.TransformData(data);
-            File.WriteAllBytes(outputPath, data);
+                    // Encrypt if requested
+                    if (encrypt) data = Encryption.TransformData(data);
+                    File.WriteAllBytes(outputPath, data);
+                }
+            }
         }
     }
 }
